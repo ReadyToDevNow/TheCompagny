cf2540b4605c77a33d6aaee80253128da2fc4b6c
ReadyToDevNow <learntolearn0@gmail.com>
2025-07-03 16:36:22 +0200

    NavigationBar and Footer

---
+++ src/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavigationBar from '@/components/NavigationBar.vue'
+import FooterBar from '@/components/FooterBar.vue'
+import MainContent from '@/components/MainContent.vue'
+</script>
+
+<template>
+  <div>
+    <NavigationBar />
+    <MainContent />
+    <FooterBar />
+  </div>
+</template>
