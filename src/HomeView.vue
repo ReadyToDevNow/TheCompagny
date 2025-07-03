8329d8f1a1d0765e4fcd09d8e505f2e025de35c1
ReadyToDevNow <learntolearn0@gmail.com>
2025-07-03 17:21:58 +0200

    about , main and portfolio sections

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -1,13 +1,17 @@
 <script setup>
 import NavigationBar from '@/components/NavigationBar.vue'
 import FooterBar from '@/components/FooterBar.vue'
-import MainContent from '@/components/MainContent.vue'
+import MainSection from './sections/MainSection.vue'
+import AboutSection from './sections/AboutSection.vue'
+import PortfolioSection from './sections/PortfolioSection.vue'
 </script>
 
 <template>
   <div>
     <NavigationBar />
-    <MainContent />
+    <MainSection />
+    <AboutSection />
+    <PortfolioSection />
     <FooterBar />
   </div>
 </template>
