9c3b49ba09bb257e6f4bdb7246fbab08c43cd5e6
ReadyToDevNow <learntolearn0@gmail.com>
2025-07-08 18:15:49 +0200

    feat(responsive): min-h-screen

--- src/sections/MainSection.vue
+++ src/sections/MainSection.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- Main section start -->
-  <section class="h-screen px-12 lg:px-54 py-54 mb-8">
-    <div class="flex flex-col lg:flex-row">
-      <div class="flex flex-col">
+  <section class="min-h-screen px-12 lg:px-54 py-54 mb-8">
+    <div class="flex flex-col md:flex-row">
+      <div class="flex flex-col mb-4">
         <!-- Main text start -->
         <h1 class="uppercase text-5xl xl:text-7xl mb-4 font-semibold z-40">
           TOI FÉMININ ESSENTIELLE
@@ -15,7 +15,7 @@
       <!-- Main text end -->
       <!-- Photographer image start absolute bottom-20 right-44 overflow-hidden h-2/5 sm:h-1/2 md:h-3/5 lg:h-4/6 -->
       <div class="z-49 lg:basis-2/3">
-        <img src="@/assets/tfe.jpg" alt="img" class="h-full rounded" />
+        <img src="@/assets/tfe.jpg" alt="img" class="h-full rounded mb-4" />
       </div>
     </div>
     <!-- Photographer image end -->
