3bd1ba18e49be95a1c97de103fa524710461ffc8
ReadyToDevNow <learntolearn0@gmail.com>
2025-07-09 13:37:48 +0200

    feat(DateSection) edit 10 dates

--- src/sections/DateSection.vue
+++ src/sections/DateSection.vue
@@ -1,55 +1,7 @@
 <template>
   <!-- Date section start -->
   <section id="date-section" class="px-12 lg:px-32 py-16 border-t border-stone-500 relative">
-    <ul class="grid grid-cols-1 lg:grid-cols-4 justify-center items-center gap-4">
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
-      <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
-        <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
-        <p class="text-sm text-gray-500">10 juillet 2025</p>
-      </li>
+    <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4">
       <li class="bg-white shadow-lg rounded-lg p-4 border-l-4 border-pink-500">
         <h4 class="text-lg font-bold text-stone-800">Paris - Théatre Darius Milhaud</h4>
         <p class="text-sm text-gray-500">10 juillet 2025</p>
